b6e013ad6f084854cf5d1d1094b82b5cf788e025
Christopher R. Merlo <cmerlo@ncc.edu>
2011-01-24 16:27:37 -0500

    Syllabus bux fix; contact info redesign

--- admin/contact_info.php
+++ admin/contact_info.php
@@ -1,83 +1,52 @@
 <?php
 
-$title_stub = 'Contact Information Editor';
+$title_stub = 'Edit Contact Information';
 require_once( '../_header.inc' );
 
 if( $_SESSION[ 'admin' ] == 1 ) {
+    $info_query = 'select contact_info, last_updated from contact_info';
+    $info_result = $db->query( $info_query );
+    $info = 'Click here to edit your contact information.  Click outside the box when you\'re done.';
+    if( $info_result->num_rows == 1 ) {
+        $info_row = $info_result->fetch_object( );
+        $info = nl2br( $info_row->contact_info );
+        $last_update = date( 'l, F j, Y \a\t g:i a', strtotime( $info_row->last_updated ) );
+    }
+    
+    print "<p>Enter or edit your contact information below.  You may want to use HTML."
+        . "  <span id=\"last_update\">";
+    if( isset( $last_update ) ) {
+        print "You last updated your contact information on "
+            . "$last_update.";
+    }
+    print "</span></p>\n";
+    print "<div style=\"border: 1px solid white; padding: 1em;\">"
+        . "<span class=\"editInPlace\" id=\"info\">$info</span></div>\n";
+        
 ?>
-<h2>Current Contact Information</h2>
-<div id="current_contact_info"></div>
-    
-<h2>Add New Information</h2>
-<div id="add_contact_info">
-    <table>
-        <tr>
-            <td>DB Key:</td>
-            <td><input type="text" id="type" /></td>
-        </tr>
 
-        <tr>
-            <td>Description:</td>
-            <td><input type="text" id="description" /></td>
-        </tr>
-
-        <tr>
-            <td>Contact Information:</td>
-            <td><textarea id="contact_info" rows="5" cols="40"></textarea></td>
-        </tr>
-
-        <tr>
-            <td>Sequence:</td>
-            <td><input type="text" id="sequence" /></td>
-        </tr>
-        
-        <tr>
-            <td colspan="2" align="center">
-                <input type="submit" value="Add This Information" id="add" />
-            </td>
-        </tr>
-    </table>
-</div>  <!-- div#add_contact_info -->
-    
 <script type="text/javascript">
-$(document).ready(function(){
-    $.post( 'list_contact_information.php',
-        function(data) {
-            $('div#current_contact_info').html(data);
-        }
-    )
     
-    $('input:submit#add').click(function(){
-        var type = $('input#type').val();
-        var description = $('input#description').val();
-        var contact_info = $('textarea#contact_info').val();
-        var sequence = $('input#sequence').val();
-        
-        $.post( 'list_contact_information.php',
-            {
-                type: type,
-                description: description,
-                contact_info: contact_info,
-                sequence: sequence
-            },
-            function(data){
-                $('div#current_contact_info').html(data);
-                $('input#type').val('');
-                $('input#description').val('');
-                $('textarea#contact_info').val('');
-                $('input#sequence').val('');
-            }
-        )
-    })
+$(document).ready(function(){
+    $("span#info").editInPlace({
+        url: "update_contact_information.php",
+        params: "ajax=yes",
+        field_type: "textarea",
+        textarea_rows: "5",
+        textarea_cols: "60",
+        saving_image: "<?php echo $docroot ?>/images/ajax-loader.gif"
+    });
 })
+    
 </script>
 
 <?php
+
 } else {
     print $no_admin;
 }
-   
+
 $lastmod = filemtime( $_SERVER[ 'SCRIPT_FILENAME' ] );
-include( "$fileroot/_footer.inc" );
+include ( "$fileroot/_footer.inc" );
 
 ?>
--- admin/update_contact_information.php
+++ admin/update_contact_information.php
@@ -7,20 +7,36 @@ if( $_SESSION[ 'admin' ] == 1 ) {
     
     $update_value = $db->real_escape_string( trim( $_POST[ 'update_value' ] ) );
     $original_html = $db->real_escape_string( trim( $_POST[ 'original_html' ] ) );
-    $id = $db->real_escape_string( trim( $_POST[ 'element_id' ] ) );
-    $column = $db->real_escape_string( trim( $_POST[ 'column' ] ) );
     
 	if( $update_value != $original_html ) {
-		$update_query = "update contact_information set $column = \"$update_value\" "
-            . "where $column = \"$original_html\"";
-		$update_result = $db->query( $update_query );
+	    $rows_query = 'select * from contact_info';
+        $rows_result = $db->query( $rows_query );
+        if( $rows_result->num_rows == 1 ) {
+    		$update_query = "update contact_info set contact_info = \"$update_value\", "
+                . "last_updated = \"" . date( 'Y-m-d H:i:s' ) . "\"";
+		    $update_result = $db->query( $update_query );
+        } else {
+            $insert_query = 'insert into contact_info ( contact_info, last_updated ) '
+                . "values( \"$update_value\", \"" . date( 'Y-m-d H:i:s' ) . "\" )";
+            $insert_result = $db->query( $insert_query );
+        }
 		if( $db->affected_rows == 1 ) {
-			$new_data_query = "select $column as x from contact_information "
-				. "where $column = \"$update_value\"";
+			$new_data_query = "select contact_info, last_updated from contact_info";
 			$new_data_result = $db->query( $new_data_query );
-            $new_data_row = $new_data_result->fetch_assoc( );
+            $new_data_row = $new_data_result->fetch_object( );
             $new_data_result->close( );
-			print $new_data_row[ 'x' ];
+			print $new_data_row->contact_info;
+            $date = date( 'l, F j, Y \a\t g:i a', strtotime( $new_data_row->last_updated ) );
+?>
+
+<script type="text/javascript">
+$(document).ready(function(){
+    $('span#last_update').html("You last updated your contact information on " +
+        "<?php echo $date; ?>." );
+})  
+</script>
+
+<?php
 		} else {
 			print $original_html;
 		}
