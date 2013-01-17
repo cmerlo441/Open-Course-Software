f9000e3e5091928585dc881eb70941bed5fc5666
Christopher R. Merlo <cmerlo@ncc.edu>
2013-01-17 14:22:55 -0500

    Update to student's view of Reference Materials.  No longer sortable, but now updates automatically.

--- student/reference_materials.php
+++ student/reference_materials.php
@@ -4,10 +4,11 @@ $title_stub = 'Reference Materials';
 require_once( '../_header.inc' );
 
 if( $_SESSION[ 'student' ] > 0 ) {
-    
+
     $section = $db->real_escape_string( $_GET[ 'section' ] );
     
-    $course_query = 'select c.id as course_id, c.dept, c.course, s.section '
+    $course_query = 'select c.id as course_id, c.dept, c.course, '
+        . 's.section, s.id as section_id '
         . 'from courses as c, sections as s '
         . 'where s.course = c.id '
         . "and s.id = $section";
@@ -15,65 +16,34 @@ if( $_SESSION[ 'student' ] > 0 ) {
     $course_row = $course_result->fetch_assoc( );
     $course_name = $course_row[ 'dept' ] . ' ' . $course_row[ 'course' ] . ' '
         . $course_row[ 'section' ];
-    
-    $ref_query = 'select * from reference '
-        . "where section = $section "
-        . "and available = 1 "
-        . 'order by filename';
-    $ref_result = $db->query( $ref_query );
-    
-    if( $ref_result->num_rows == 0 ) {
-        print 'None.';
-    } else {
-  
 ?>
-
-<table id="reference" class="tablesorter">
-    <thead>
-        <tr>
-            <th>Filename</th>
-            <th>Size</th>
-            <th>Upload Date</th>
-        </tr>
-    </thead>
     
-    <tbody>
-<?
-        while( $ref = $ref_result->fetch_assoc( ) ) {
-            print "        <tr id=\"{$ref[ 'id' ]}\">\n";
-            print "            <td>{$ref[ 'filename' ]}</td>\n";
-            print "            <td>{$ref[ 'size' ]} Bytes</td>\n";
-            print "            <td>"
-                . date( 'F j, Y g:i A', strtotime( $ref[ 'uploaded' ] ) ) . "</td>\n";
-            print "        </tr>\n";
-        }
-?>
-    </tbody>
-</table>
-
-<?
-    }
-?>
+<div id="ref_list"></div>  <!-- div#ref_list -->
 
 <script type="text/javascript">
 $(document).ready(function(){
     var course = " :: <?php echo $course_name; ?>";
+    var section = "<?php echo $course_row[ 'section_id' ]; ?>";
     
     $('h1').html( $('h1').html( ) + course );
     $(document).attr('title', $(document).attr('title') + course );
-    $('#reference').tablesorter({
-        headers: { 1: { sorter: 'digit' },
-                   2: { sorter: 'date' } },
-        sortList: [ [2,1], [0,0] ],
-        widgets: [ 'ocsw', 'clickable_rows' ]
-    });
     
-    $('#reference tr').click(function(){
-        var id = $(this).attr('id');
-        
-        window.open("../download_reference_material.php?id=" + id );
-        return false;
-    })
+    $.post( 'list_reference_materials.php',
+        { section: section },
+        function( data ) {
+            $('div#ref_list').html(data);
+        }
+    )
+
+    setInterval( function(){
+        $.post( 'list_reference_materials.php',
+            { section: section },
+            function( data ) {
+                $('div#ref_list').html(data);
+            }
+        )
+    }, 2500 );
+
 })
 </script>
 
